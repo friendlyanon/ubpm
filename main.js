9cabdd7a142d2fc1d6a7c4603d6f1abe236e12da
friendlyanon <friendlyanon@users.noreply.github.com>
2021-01-13 13:35:32 +0000

    Make the repository a GitHub action
    This action will copy the ubpm.cmake file to a given directory and
    output its location for use in a later step.

---
+++ main.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const process = require("process");
+const path = require("path");
+const fs = require("fs");
+const { EOL } = require("os");
+
+const map = new Map([
+  ["%", "%25"],
+  ["\r", "%0D"],
+  ["\n", "%0A"],
+]);
+
+const regex = /[%\r\n]/g;
+const replacer = Map.prototype.get.bind(map);
+
+const moduleName = "ubpm.cmake";
+
+try {
+  const destination = process.env.INPUT_DESTINATION || ".ubpm_root";
+  const fromLocation = path.join(__dirname, moduleName);
+  const locationParent = path.join(process.cwd(), destination);
+  const location = path.join(locationParent, moduleName);
+  fs.mkdirSync(locationParent, { recursive: true });
+  fs.copyFileSync(fromLocation, location);
+
+  const value = location.replace(regex, replacer);
+  process.stdout.write(`::set-output name=location::${value}${EOL}`);
+} catch (error) {
+  process.exitCode = 1;
+  process.stdout.write(`::error::${error.message}${EOL}`);
+}
